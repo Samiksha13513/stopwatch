d6434bf549edb44e1476cb354c888f6f04efe512
Samiksha <samiksha@thoughtwin.com>
2025-03-06 14:01:35 +0530

    add resume functionality and set start button

--- script.js
+++ script.js
@@ -2,47 +2,44 @@ let hrs = 0;
 let min = 0;
 let sec = 0;
 let timeinterval;
-
+let count = 0;
 function start() {
-  let count = 0;
-//   if (timeinterval) {
-//     return;
-//   }
-  timeinterval = setInterval(() => {
-    count++;
+  if (!timeinterval) {
+    timeinterval = setInterval(() => {
+      count++;
 
-    document.getElementById("second").innerHTML = count;
+      document.getElementById("second").innerHTML = count;
+      document.getElementById("minute").innerHTML = min;
 
-    document.getElementById("minute").innerHTML = min;
+      document.getElementById("hour").innerHTML = hrs;
 
-    document.getElementById("hour").innerHTML = hrs;
-
-    if (count === 60) {
-      count = 0;
-      min++;
-      if (min == 60) {
-        min = 0;
-        hrs++;
+      if (count === 60) {
+        count = 0;
+        min++;
+        if (min == 60) {
+          min = 0;
+          hrs++;
+        }
       }
-    }
-  }, 100);
+    }, 100);
+  }
 }
 function pause() {
-  clearInterval(timeinterval);
-//   alert("pause");
+  if (timeinterval) {
+    clearInterval(timeinterval);
+    timeinterval = null;
+  }
 }
-// function resume(){
 
-//  }
 function reset() {
   clearInterval(timeinterval);
-
+  timeinterval = null;
   hrs = 0;
   min = 0;
   sec = 0;
+  count = 0;
   document.getElementById("second").innerHTML = 0;
   document.getElementById("minute").innerHTML = 0;
 
   document.getElementById("hour").innerHTML = 0;
-//   alert("reset");
 }
